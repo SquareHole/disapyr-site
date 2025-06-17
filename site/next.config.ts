35a0fcec55877bcf8cb906c15e1d652b1126db32
Johan van Rhyn <johan@squarehole.dev>
2025-06-17 20:11:41 +0200

    Applied changes to implement the CSP extension of Netlify

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value:
-      "default-src 'self'; script-src 'self' ; object-src 'none'; base-uri 'none'; frame-ancestors 'none';",
-  },
-];
-
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
